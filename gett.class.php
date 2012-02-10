00382d9ac9a3e877dabae35d8a6635c9b9e41869
Jacob Gross <kurtextrem@gmail.com>
2012-02-10 08:00:51 +0100

    ge.tt API without cURL

---
+++ gett.class.php
@@ -0,0 +1,122 @@
+<?php
+set_time_limit(0);
+error_reporting(E_ALL);
+
+/**
+ * ge.tt PHP Library
+ * Easily interact with ge.tt API via PHP.
+ * Website:		http://www.spanlayer.com
+ *
+ * @version		1.0.0
+ * @author		Ahmed Hosny <admin@spanlayer.com>, Jacob Groß / kurtextrem <kurtextrem@gmail.com>
+ * 
+ * @license		This library is Free for everyone to use or modify,
+ * 			Keeping the original credits,
+ * 			Appending notes and credits for any modifications.
+ */
+class gett {
+
+	/**
+	 * Your ge.tt API Key
+	 *
+	 * @var		string
+	 */
+	private $APIKey;
+
+	/**
+	 * ge.tt Access Token
+	 *
+	 * @var		mixed
+	 */
+	private $accessToken;
+
+	/**
+	 * Loads the "Request" Lib
+	 *
+	 * @param	string		$APIKey
+	 */
+	public function __construct($APIKey) {
+		session_start();
+		require_once 'library/Requests.php';
+		Requests::register_autoloader();
+
+		$this->APIKey = $APIKey;
+	}
+
+	/**
+	 * REST API Connection to ge.tt
+	 *
+	 * @param	string		$method
+	 * @param	array		$postData
+	 * @return	mixed
+	 * @throws	Exception
+	 */
+	public function request($method, $postData) {
+		$post_data_str = json_encode($postData);
+		$this->accessToken = $this->accessToken ? $this->accessToken : (isset($_SESSION['gett_accessToken']) ? $_SESSION['gett_accessToken'] : false);
+
+		$accessToken = $this->accessToken ? '?accesstoken=' . $this->accessToken : '';
+		try {
+			if (is_array($postData) && count($postData) > 0) {
+				$request = Requests::post('https://open.ge.tt/1/' . $method . $accessToken, array(), $post_data_str);
+			} else {
+				$request = Requests::get('https://open.ge.tt/1/' . $method . $accessToken);
+			}
+		} catch (Exception $e) {
+			$this->handleException($e);
+		}
+
+		$result = json_decode($request->body);
+
+		try {
+			if (isset($result->error))
+				throw new Exception('<i>Error:</i> <strong>' . $result->error . '</strong>', E_USER_ERROR);
+			elseif (is_null($result))
+				throw new Exception('<strong>Unknown method <i>(' . $method . ')</i> or <span style="color: white; background-color: lightblue">GE.TT</span> API is down!</strong>', E_USER_ERROR);
+		} catch (Exception $e) {
+			$this->handleException($e);
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Auth process
+	 *
+	 * @param	string		$email
+	 * @param	string		$password
+	 * @return	bool
+	 * @throws	Exception
+	 */
+	public function auth($email, $password) {
+		try {
+			if (isset($_SESSION['gett_accessToken']) || !empty($this->accessToken))
+				return false;
+			// throw new Exception($email . ' is already authed.'); // There's no problem if user is already authed
+		} catch (Exception $e) {
+			$this->handleException($e);
+		}
+
+
+		$user = $this->request('users/login', array(
+		    'apikey' => $this->APIKey,
+		    'email' => $email,
+		    'password' => $password
+			));
+
+		$_SESSION['gett_accessToken'] = $user->accesstoken;
+		$this->accessToken = $user->accesstoken;
+
+		return true;
+	}
+
+	/**
+	 * Handles caught exceptions
+	 *
+	 * @param	Exception	$e
+	 */
+	private function handleException($e) {
+		echo '<div class="gettError"><span style="color: white; background-color: lightblue">GE.TT</span> ' . $e->getMessage() . '<br><code>in <b>' . $e->getFile() . '</b> on line <b>' . $e->getLine() . '</b></code></div>';
+	}
+
+}
---
+++ test.php
@@ -0,0 +1,42 @@
+﻿<?php
+/**
+ * Example file showing how we can use the library.
+ * 
+ * Any questions? https://github.com/kurtextrem/gett-php/issues
+ */
+require_once('gett.class.php');
+
+/*
+ * APIKey is required
+ * 
+ * @see http://ge.tt/developers
+ */
+$gett = new gett('API');
+
+/**
+ * Authenticate using your own e-mail and password (Must be registered on ge.tt).
+ */
+$gett->auth('e-mail', 'password');
+
+
+/*
+ * We'd send requests by calling `request` method.
+ *
+ *  First Argument: 	(required) Method name to execute,
+ * 					For list of available methods, refer to: http://ge.tt/developers
+ * Second Argument: (optional) Holds any parameters to be sent with the request.
+ * 					Passing parameters convert the request type to POST.
+ */
+
+/*
+ * Get list of files you own.
+ */
+$my_shares = $gett->request('shares');
+var_dump($my_shares);
+
+/*
+ * Creating new share titled 'New Share'.
+ * Method `request` here would send POST Request as there're some parameters passed in the second argument.
+ */
+$new_share = $gett->request('shares/create', Array('title'=>'New Share !'));
+var_dump($new_share);
